30580395851b396ee79fb91c5774ef39bbfb7bd2
Alexandre Sinicio <alesinicio@gmail.com>
2025-05-05 11:30:12 -0300

    Feat: add TTL to AsyncTaskDTO

--- src/AsyncTaskDTO.php
+++ src/AsyncTaskDTO.php
@@ -2,10 +2,13 @@
 
 namespace Alesinicio\AsyncExecutor;
 
+use DateInterval;
+
 readonly class AsyncTaskDTO {
 	public function __construct(
-		public string $class,
-		public array  $args = [],
+		public string        $class,
+		public array         $args = [],
+		public ?DateInterval $ttl = null,
 	) {}
 	public static function fromArray(array $input) : ?self {
 		$class = $input['class'] ?? null;
@@ -14,7 +17,10 @@ readonly class AsyncTaskDTO {
 		$args = $input['args'] ?? [];
 		if (!is_array($args)) return null;
 
-		return new self($class, $args);
+		$ttl = $input['ttl'] ?? null;
+		if ($ttl && (!$ttl instanceof DateInterval)) return null;
+
+		return new self($class, $args, $ttl);
 	}
 	public function asArray() : array {
 		return (array)$this;
