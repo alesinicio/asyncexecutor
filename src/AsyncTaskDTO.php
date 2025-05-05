e4254ebb692eb476e26a12782e1f9ff7380e4002
Alexandre Sinicio <alesinicio@gmail.com>
2025-05-05 12:33:33 -0300

    Fix: typo

--- src/AsyncTaskDTO.php
+++ src/AsyncTaskDTO.php
@@ -3,6 +3,7 @@
 namespace Alesinicio\AsyncExecutor;
 
 use DateInterval;
+use DateMalformedIntervalStringException;
 
 readonly class AsyncTaskDTO {
 	public function __construct(
@@ -18,9 +19,29 @@ readonly class AsyncTaskDTO {
 		if (!is_array($args)) return null;
 
 		$ttl = $input['ttl'] ?? null;
-		if ($ttl && (!$ttl instanceof DateInterval)) return null;
+		if (is_array($ttl)) $ttl = static::createDateIntervalFromArray($ttl);
 
 		return new self($class, $args, $ttl);
+	}
+	private static function createDateIntervalFromArray(array $data) : ?DateInterval {
+		$y = $data['y'] ?? 0;
+		$m = $data['m'] ?? 0;
+		$d = $data['d'] ?? 0;
+		$h = $data['h'] ?? 0;
+		$i = $data['i'] ?? 0;
+		$s = $data['s'] ?? 0;
+
+		$invert       = $data['invert'] ?? 0;
+		$intervalSpec = 'P' . $y . 'Y' . $m . 'M' . $d . 'DT' . $h . 'H' . $i . 'M' . $s . 'S';
+		try {
+			$interval = new DateInterval($intervalSpec);
+		} catch (DateMalformedIntervalStringException) {
+			return null;
+		}
+
+		$interval->invert = $invert;
+
+		return $interval;
 	}
 	public function asArray() : array {
 		return (array)$this;
